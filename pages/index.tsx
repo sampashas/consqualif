ed259b7c9c9bae7d4195d9c22fd08f16146665e2
Pavel S <sampashass@gmail.com>
2024-09-09 04:17:49 +0700

    consqualif

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { useContext, useRef } from "react";
 import Title from "../components/Title";
 import useMobile from "../hooks/useMobile";
 import SContext from "../contexts/SContext";
+import { motion } from "framer-motion";
+import { config } from "../styles/global";
 
 const Home = () => {
   const { ref } = useContext(SContext);
@@ -15,9 +17,21 @@ const Home = () => {
         data-scroll-id
         ref={ref}
         id="main-container"
-        className="will-change-scroll"
+        className="will-change-scroll overflow-hidden"
       >
-        Content
+        <motion.div
+          initial={{ scale: 1.1 }}
+          animate={{ scale: 1 }}
+          transition={{ ease: config.animations.speed, duration: 1 }}
+          className="w-full h-screen"
+        >
+          <div className="bg-black opacity-50 absolute h-screen w-full"></div>
+          <img
+            className="h-screen object-cover"
+            src="/slider/1.png"
+            alt=""
+          ></img>
+        </motion.div>
       </main>
     </>
   );
