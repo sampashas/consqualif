810736b0896ea86ed10e5de48b50c0cd2d2b85cd
Pavel S <sampashass@gmail.com>
2024-09-09 04:21:16 +0700

    cookie

--- components/Cookie.tsx
+++ components/Cookie.tsx
@@ -35,28 +35,28 @@ const CookieConsentBanner: React.FC<CookieConsentBannerProps> = ({
   if (!isBannerVisible) return null;
 
   return (
-    <div className="fixed z-10 left-0 right-0 bottom-0 items-center md:bottom-3 w-full flex justify-center">
+    <div className="fixed z-10 left-0 right-0 bottom-0 items-center md:bottom-3 w-full flex">
       <AnimatePresence mode="wait">
         <motion.div
           initial="toUp"
           animate="visible"
           exit="toDown"
           transition={{ ease: config.animations.speed, delay: 1 }}
-          className="md:w-fit gap-4 w-full py-4 px-6 md:mx-6 flex items-center md:rounded-2xl bg-[#00000070] border border-wh15 backdrop-blur-3xl mix-blend-difference "
+          className="md:w-fit gap-4 w-full py-4 px-6 md:mx-6 flex items-center md:rounded-xl bg-[#00000070] border border-wh15 backdrop-blur-3xl mix-blend-difference "
         >
           <CookieText />
           <div className="flex items-end justify-end gap-2 md:w-full w-fit md:justify-center">
             <button
               aria-label="close"
               onClick={handleDecline}
-              className="btn text-white border bg-white/20 w-14 h-8 px-2 md:w-full"
+              className="btn text-white bg-[#fff2] border-[#fff2] border-[1px] rounded-md w-14 h-8 px-2 md:w-full"
             >
               <X />
             </button>
             <button
               aria-label="aprove"
               onClick={handleConsent}
-              className="btn text-white border bg-white/20 w-14 h-8 px-2 md:w-full"
+              className="btn text-white bg-[#fff2] border-[#fff2] border-[1px] rounded-md w-14 h-8 px-2 md:w-full"
             >
               <Check />
             </button>
