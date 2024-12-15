4651884b3069d768e00a8dbe5a56227f3fcee33d
Pavel S <sampashass@gmail.com>
2024-12-16 05:19:57 +0700

    header mobi

--- components/Base/Header.tsx
+++ components/Base/Header.tsx
@@ -11,6 +11,24 @@ function Header() {
     <div className="z-[3] fixed my-0 pxes w-full">
       <nav className="w-full myes md:pt-0 mn:pt-2 flex justify-between items-center">
         <LeftNav />
+        <svg
+          height="24"
+          className="h-[2.5em] md:hidden"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g opacity="0.59">
+            <path
+              d="M21 11.5C21.0034 12.8199 20.6951 14.1219 20.1 15.3C19.3944 16.7117 18.3098 17.8992 16.9674 18.7293C15.6251 19.5594 14.0782 19.9994 12.5 20C11.1801 20.0034 9.87812 19.6951 8.7 19.1L3 21L4.9 15.3C4.30493 14.1219 3.99656 12.8199 4 11.5C4.00061 9.92176 4.44061 8.37485 5.27072 7.03255C6.10083 5.69025 7.28825 4.60557 8.7 3.9C9.87812 3.30493 11.1801 2.99656 12.5 3H13C15.0843 3.11499 17.053 3.99476 18.5291 5.47086C20.0052 6.94695 20.885 8.91565 21 11V11.5Z"
+              stroke="white"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+        </svg>
+
         <Link href="/">
           <Logo />
         </Link>
@@ -97,30 +115,32 @@ function LeftNav() {
   }, [route.asPath]);
 
   return (
-    <ul className="mn:hidden md:flex h-fit flex-1 gap-8">
-      {leftSide.map((obj, id) => (
-        <li className="overflow-hidden h-fit">
-          <Link href={obj.link}>
-            <motion.div
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              transition={{
-                ease: config.animations.speed,
-                duration: 1,
-                delay: 1.7 + id * -0.1,
-              }}
-            >
-              <span className={`${activeLink === obj.link ? "" : "hover"} `}>
-                {obj.name}
-              </span>
-              {activeLink === obj.link && (
-                <div className="w-full translate-y-[-.1em] h-[1px] bg-white" />
-              )}
-            </motion.div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="mn:hidden md:flex h-fit flex-1 gap-8">
+        {leftSide.map((obj, id) => (
+          <li className="overflow-hidden h-fit">
+            <Link href={obj.link}>
+              <motion.div
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                transition={{
+                  ease: config.animations.speed,
+                  duration: 1,
+                  delay: 1.7 + id * -0.1,
+                }}
+              >
+                <span className={`${activeLink === obj.link ? "" : "hover"} `}>
+                  {obj.name}
+                </span>
+                {activeLink === obj.link && (
+                  <div className="w-full translate-y-[-.1em] h-[1px] bg-white" />
+                )}
+              </motion.div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 function RightNav() {
